a4824c0d2b67571580c3a3183db8ce62f0c142f1
Marcin Dawidziuk <cinekdawidziuk@gmail.com>
2020-04-27 22:33:19 +0100

    Tailwind room redesign

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -32,7 +32,18 @@ module.exports = (env, options) => (
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: [
+                require('tailwindcss'),
+                require('autoprefixer'),
+              ],
+          },
+          }
+          ]
       }
     ]
   },
