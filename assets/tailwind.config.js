e953eb58fa1bf8da468c801fcd0dea28f9d48aee
Marcin Dawidziuk <cinekdawidziuk@gmail.com>
2020-04-30 21:57:17 +0100

    UI fixes

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
+    theme: {
+        extend: {
+            gridTemplateColumns: {
+                '14': 'repeat(14, minmax(0, 1fr))',
+            },
+        },
+        variants: {
+            gridTemplateColumns: ['responsive', 'hover', 'focus'],
+        },
+        plugins: [],
+    }
 }
