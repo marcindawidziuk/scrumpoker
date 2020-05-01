c790f32a99b02b72339c30e0716011e0fa55a769
Marcin Dawidziuk <cinekdawidziuk@gmail.com>
2020-05-01 13:11:49 +0100

    Started on dark theme

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,13 +1,9 @@
+const tailwind = require('tailwindcss/defaultTheme');
+
 module.exports = {
-    theme: {
-        extend: {
-            gridTemplateColumns: {
-                '14': 'repeat(14, minmax(0, 1fr))',
-            },
-        },
-        variants: {
-            gridTemplateColumns: ['responsive', 'hover', 'focus'],
-        },
-        plugins: [],
-    }
-}
+    theme: {},
+    variants: {},
+    plugins: [
+        require('./theme.config.js')
+    ],
+};
---
+++ assets/theme.config.js
@@ -0,0 +1,49 @@
+﻿const { ThemeBuilder, Theme } = require('tailwindcss-theming');
+
+const mainTheme = new Theme()
+    .default()
+    .assignable()
+    .colors({
+        'accent':'#667eea',
+        'secondary':'#ebf4ff',
+        'third':'#ffffff',
+        'on-secondary':'#ffffff',
+        'brand':'rgb(90, 103, 216)',
+        'on-brand':'#ffffff',
+        'text-black': '#ffffff'
+    });
+
+const lightTheme = new Theme()
+    .name('light')
+    .assignable()
+    .colors( mainTheme.getColors());
+
+const darkTheme = new Theme()
+    .dark()
+    .colors({
+        'accent':'#3b3b78',
+        'secondary':'#1f2023',
+        'third':'#000000',
+        'on-secondary':'#ffffff',
+        'brand':'#ff0000',
+        'on-brand':'#ffffff',
+        'text-black': '#00000f'
+    })
+    .assignable();
+
+// const lightTheme = new Theme()
+//     .light()
+//     .colors({
+//         'accent':'#667eea',
+//         'brand':'rgb(90, 103, 216)',
+//         'on-brand':'#ffffff',
+//         'text-black': '#ffffff'
+//     })
+// ;
+//
+
+module.exports = new ThemeBuilder()
+    .asDataThemeAttribute()
+    .default(mainTheme)
+    .light(lightTheme, true, true)
+    .dark(darkTheme);
