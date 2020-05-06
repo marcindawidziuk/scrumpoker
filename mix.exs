53883dc8dfdaa960a8e4d5a08a84689cd34b025b
Marcin Dawidziuk <cinekdawidziuk@gmail.com>
2020-05-06 12:40:49 +0100

    Updated to Phoenix 1.5

--- mix.exs
+++ mix.exs
@@ -33,14 +33,13 @@ defmodule Scrumpoker.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.14"},
-      {:phoenix_pubsub, "~> 1.1"},
-      {:postgrex, ">= 0.0.0"},
+      {:phoenix, "~> 1.5.1"},
+      {:phoenix_pubsub, "~> 2.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.2"}
     ]
   end
 
