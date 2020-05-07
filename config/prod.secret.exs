f33ed348f1ab9621242db56e2cf85deaac36d4ef
Marcin Dawidziuk <cinekdawidziuk@gmail.com>
2020-05-07 13:57:25 +0100

    Fixed release

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -35,7 +35,7 @@ config :scrumpoker, ScrumpokerWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :scrumpoker, ScrumpokerWeb.Endpoint, server: true
+config :scrumpoker, ScrumpokerWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
