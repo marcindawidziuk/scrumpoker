39161c235b59998b8f78a6da72c52b59d2ae774a
Marcin Dawidziuk <cinekdawidziuk@gmail.com>
2022-04-13 19:15:50 +0100

    Default to light theme

--- ui/nuxt.config.ts
+++ ui/nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
         '@nuxtjs/color-mode'
     ],
     colorMode: {
-        preference: 'system', // default value of $colorMode.preference
+        preference: 'light', // default value of $colorMode.preference
         fallback: 'light', // fallback value if not system preference found
         hid: 'nuxt-color-mode-script',
         globalName: '__NUXT_COLOR_MODE__',
